e77c79c6f85cc6b90b6c38aa15ddf3d6979780d9
Heba <dr.hebaelgamil@gmail.com>
2024-02-07 21:01:20 -0600

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app=express();
+dotenv.config()
 
-app.listen(3000, ()=>{
-    console.log('server is running on port 3000');
-})
+const username = process.env.MONGODB_USERNAME;
+const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+const cluster = process.env.MONGODB_CLUSTER;
+const database = process.env.MONGODB_DATABASE;
+
+const uri = `mongodb+srv://${username}:${password}@${cluster}.uidoptj.mongodb.net/${database}?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.log(err));
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
