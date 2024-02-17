ee519afeb31918798b47ed651b995cbda5091467
Heba <dr.hebaelgamil@gmail.com>
2024-02-17 16:24:50 -0600

    add signup page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,63 +1,112 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, json, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields.");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/sign-up", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      console.log(res); // Log the response
+      const data = await res.json();
+      if (data.success === false) {
+        setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      console.error(error); // Log any catch errors
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
   return (
-    <div className='min-h-screen mt-20'>
-      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* left */}
-        <div className='flex-1'>
-          <Link to='/' className='font-bold dark:text-white text-4xl'>
-            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
               Heba's
             </span>
             Blog
           </Link>
-          <p className='text-sm mt-5'>
+          <p className="text-sm mt-5">
             This is a demo project. You can sign up with your email and password
             or with Google.
           </p>
         </div>
         {/* right */}
 
-        <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
-              <Label value='Your username' />
+              <Label value="Your username" />
               <TextInput
-                type='text'
-                placeholder='Username'
-                id='username'
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Your email' />
+              <Label value="Your email" />
               <TextInput
-                type='email'
-                placeholder='name@company.com'
-                id='email'
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
               />
             </div>
             <div>
-              <Label value='Your password' />
+              <Label value="Your password" />
               <TextInput
-                type='password'
-                placeholder='Password'
-                id='password'
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
               />
             </div>
             <Button
-              gradientDuoTone='purpleToPink'
-              type='submit'
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
             >
-              Sign up
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
-            
           </form>
-          <div className='flex gap-2 text-sm mt-5'>
+          <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to='/sign-in' className='text-blue-500'>
+            <Link to="/sign-in" className="text-blue-500">
               Sign In
             </Link>
           </div>
