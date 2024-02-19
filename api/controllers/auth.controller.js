8cd7e6602ef3ec6d6ca35d82957bad605f731b8a
Heba <dr.hebaelgamil@gmail.com>
2024-02-19 13:43:13 -0600

    add signin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/errorHandling.js";
+import Jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -29,3 +30,32 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin= async(req,res,next)=>{
+  const {email, password}=req.body;
+
+  if(!email || !password || email=="" || password==""){
+    return next(errorHandler(400, "All fields are required !"))
+  }
+
+  try {
+    const validUser= await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(400, 'Check your credentials'))
+    }
+
+    const validPassword= bcryptjs.compareSync(password, validUser.password);
+    if(!validPassword){
+      return next(errorHandler(400, 'Check your credentials'))
+    }
+
+    const token= Jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+
+    res.status(200).cookie('access_token', token, {httpOnly:true}).json('Valid User')
+
+    
+  } catch (error) {
+    return next(error)
+  }
+
+}
